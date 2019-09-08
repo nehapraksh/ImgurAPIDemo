9c40bf9c2884c1515d740a9f420c3bc3f863c0de
nehapraksh <neha.praksh@gmail.com>
2019-09-09 00:12:21 +1000

    removed secret key

--- app/src/main/java/com/nehaimgurdemoapp/utils/Constants.java
+++ app/src/main/java/com/nehaimgurdemoapp/utils/Constants.java
@@ -8,7 +8,7 @@ public class Constants {
 
     public static final String CLIENT_ID = "05609cb15d31f4e";
 
-    public static final String CLIENT_SECRET = "1019fcfa3bd0d97ebfc8cd06ca43f15ec6feda0a";
+
     public static final String TOP = "top";
     public static final String WEEK = "week";
     public static final String VIRAL = "viral";
